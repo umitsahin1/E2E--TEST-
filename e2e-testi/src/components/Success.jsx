688acfc208f36669ea5ae3f2c459e7249cb598dc
umitsahin <umitsahin21@gmail.com>
2024-11-28 16:42:49 +0300

    login.jsx and success.jsx done 2
    success sayfasına geri dönmek için anasayfa butornu eklendi

--- e2e-testi/src/components/Success.jsx
+++ e2e-testi/src/components/Success.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Success() {
+  const history = useHistory();
   return (
-    <div>
+    <div className="success">
       <h2>Sisteme başarı ile giriş yaptınız</h2>
+      <Button color="primary" onClick={() => history.push("/")}>
+        Ana Sayfa
+      </Button>
     </div>
   );
 }
